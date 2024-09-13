2ee39cb99a626182e9ecfc6671c7f70876cead87
FILIPE MOURA DA SILVA <filipe.moura@a.ucb.br>
2024-09-13 10:32:10 -0300

    deu certo essa merda

--- web_app/app_web1/index.js
+++ web_app/app_web1/index.js
@@ -1,7 +1,24 @@
 const lc = require('./letter_case/letter_case');
 
-let s1 = 'AbCdE';
+const express = require('express');
 
-console.log(lc.to_upper(s1));
-console.log(lc.to_lower(s1));
-console.log(lc.to_lower(1.5));
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello world, rodou porraaaaaaa');
+})
+
+app.get('/upper/:n', function(req, res){
+    let word = req.params.n;
+    res.send(lc.to_upper(word));
+})
+
+app.get('/lower/:n', function(req, res){
+    let word = req.params.n;
+    res.send(lc.to_lower(word));
+})
+
+const PORT = 8080;
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+});
