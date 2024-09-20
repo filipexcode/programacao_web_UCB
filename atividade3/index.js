06c94cfd3f42af35393fde9e4a8dacc89f07e67a
filipe <filipesilva1503code@gmail.com>
2024-09-19 22:47:14 -0300

    adicionando tarefa 3

---
+++ atividade3/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const calculadora = require('./util/calculadora');
+
+const app = express();
+
+// Rotas
+app.get('/somar/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+    const resultado = calculadora.somar(Number(a), Number(b));
+    res.send(`Resultado da soma: ${resultado}`);
+});
+
+app.get('/subtrair/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+    const resultado = calculadora.subtrair(Number(a), Number(b));
+    res.send(`Resultado da subtração: ${resultado}`);
+});
+
+app.get('/multiplicar/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+    const resultado = calculadora.multiplicar(Number(a), Number(b));
+    res.send(`Resultado da multiplicação: ${resultado}`);
+});
+
+app.get('/dividir/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+    if (Number(b) === 0) {
+        return res.status(400).send('Erro: Divisão por zero.');
+    }
+    const resultado = calculadora.dividir(Number(a), Number(b));
+    res.send(`Resultado da divisão: ${resultado}`);
+});
+
+const PORT = 8080;
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ atividade3/util/calculadora.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    somar: (a, b) => a + b,
+    subtrair: (a, b) => a - b,
+    multiplicar: (a, b) => a * b,
+    dividir: (a, b) => a / b
+};
